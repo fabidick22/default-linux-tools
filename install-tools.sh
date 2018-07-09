8d884193dbd714168a5c992a481df22e71e39aa9
by-default <exploit@localhost.localdomain>
2018-07-09 15:36:16 -0500

    funtion for check tools

---
+++ install-tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Run install tools"
+sleep 2
+tools_to_install=('tmux' 'htop');
+echo "Total tools: ${#array[*]}";
+
+check_tool(){
+	if [ $1 ]; then
+	    echo "installed";
+	else
+	    echo "no installed"
+	fi
+}
+
+for tool in ${tools_to_install[*]}
+do
+    check_tool $tool
+done
+
+#check_tool "tmux"
+#echo "Array size: ${#array[*]}"
+#sh -c `$1`
